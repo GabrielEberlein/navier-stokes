526d11d88d6174b369ffdbc288fa35c7a4e4daf3
geberlein <geberlein@dcc.fceia.unr.edu.ar>
2025-03-23 19:48:52 -0300

    testing compiler optimization

--- Makefile
+++ Makefile
@@ -1,7 +1,8 @@
 CC=gcc
-CFLAGS=-std=c11 -Wall -Wextra -Wno-unused-parameter
+CFLAGS=-std=c11 -Wall -Wextra -Wno-unused-parameter -march=$(MARCH) -O$(OPTIMIZE)
 LDFLAGS=
-
+OPTIMIZE=0
+MARCH=x86-64
 TARGETS=demo headless
 SOURCES=$(shell echo *.c)
 COMMON_OBJECTS=solver.o wtime.o
@@ -10,29 +11,29 @@ COMMON_OBJECTS=solver.o wtime.o
 # ARGUMENTOS DE LA SIMULACION
 ARGS = $(N) $(DT) $(DIFF) $(VISC) $(FORCE) $(SOURCE)
 
-N = 64  			# Tamaño de la grilla
-DT = 0.1			# Paso de tiempo
-DIFF = 0.0			# Indice de difusion
-VISC = 0.0			# Indice de viscosidad
-FORCE = 5.0			# Fuerza de la fuente
-SOURCE = 100.0		# Tasa de la fuente
+N = 64                          # Tamaño de la grilla
+DT = 0.1                        # Paso de tiempo
+DIFF = 0.0                      # Indice de difusion
+VISC = 0.0                      # Indice de viscosidad
+FORCE = 5.0                     # Fuerza de la fuente
+SOURCE = 100.0          # Tasa de la fuente
 
 all: $(TARGETS)
 
 demo: demo.o $(COMMON_OBJECTS)
-	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS) -lGL -lGLU -lglut
+        $(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS) -lGL -lGLU -lglut
 
 headless: headless.o $(COMMON_OBJECTS)
-	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
+        $(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
 
 run:
-	./demo $(ARGS)
+        ./headless $(ARGS)
 
 clean:
-	rm -f $(TARGETS) *.o .depend *~
+        rm -f $(TARGETS) *.o .depend *~
 
 .depend: *.[ch]
-	$(CC) -MM $(SOURCES) >.depend
+        $(CC) -MM $(SOURCES) >.depend
 
 -include .depend
 
--- headless.c
+++ headless.c
@@ -7,9 +7,9 @@
 
    Description:
 
-	This code is a simple prototype that demonstrates how to use the
-	code provided in my GDC2003 paper entitles "Real-Time Fluid Dynamics
-	for Games". This code uses OpenGL and GLUT for graphics and interface
+        This code is a simple prototype that demonstrates how to use the
+        code provided in my GDC2003 paper entitles "Real-Time Fluid Dynamics
+        for Games". This code uses OpenGL and GLUT for graphics and interface
 
   =======================================================================
 */
@@ -36,6 +36,8 @@ static float force, source;
 
 static float *u, *v, *u_prev, *v_prev;
 static float *dens, *dens_prev;
+
+static long int total_cells_processed = 0.0;
 
 
 /*
@@ -129,37 +131,40 @@ static void react(float* d, float* u, float* v)
 
 static void one_step(void)
 {
-    static int times = 1;
-    static double start_t = 0.0;
-    static double one_second = 0.0;
-    static double react_ns_p_cell = 0.0;
-    static double vel_ns_p_cell = 0.0;
-    static double dens_ns_p_cell = 0.0;
+    // static int times = 1;
+    // static double start_t = 0.0;
+    // static double one_second = 0.0;
+    // static double react_ns_p_cell = 0.0;
+    // static double vel_ns_p_cell = 0.0;
+    // static double dens_ns_p_cell = 0.0;
 
-    start_t = wtime();
+    // start_t = wtime();
     react(dens_prev, u_prev, v_prev);
-    react_ns_p_cell += 1.0e9 * (wtime() - start_t) / (N * N);
+    total_cells_processed += N * N;
+    // react_ns_p_cell += 1.0e9 * (wtime() - start_t) / (N * N);
 
-    start_t = wtime();
+    // start_t = wtime();
     vel_step(N, u, v, u_prev, v_prev, visc, dt);
-    vel_ns_p_cell += 1.0e9 * (wtime() - start_t) / (N * N);
+    //total_cells_processed += N * N;
+    // vel_ns_p_cell += 1.0e9 * (wtime() - start_t) / (N * N);
 
-    start_t = wtime();
+    // start_t = wtime();
     dens_step(N, dens, dens_prev, u, v, diff, dt);
-    dens_ns_p_cell += 1.0e9 * (wtime() - start_t) / (N * N);
+    //total_cells_processed += N * N;
+    // dens_ns_p_cell += 1.0e9 * (wtime() - start_t) / (N * N);
 
-    if (1.0 < wtime() - one_second) { /* at least 1s between stats */
-        printf("%lf, %lf, %lf, %lf: ns per cell total, react, vel_step, dens_step\n",
-               (react_ns_p_cell + vel_ns_p_cell + dens_ns_p_cell) / times,
-               react_ns_p_cell / times, vel_ns_p_cell / times, dens_ns_p_cell / times);
-        one_second = wtime();
-        react_ns_p_cell = 0.0;
-        vel_ns_p_cell = 0.0;
-        dens_ns_p_cell = 0.0;
-        times = 1;
-    } else {
-        times++;
-    }
+    // if (1.0 < wtime() - one_second) { /* at least 1s between stats */
+    //     printf("%lf, %lf, %lf, %lf: ns per cell total, react, vel_step, dens_step\n",
+    //            (react_ns_p_cell + vel_ns_p_cell + dens_ns_p_cell) / times,
+    //            react_ns_p_cell / times, vel_ns_p_cell / times, dens_ns_p_cell / times);
+    //     one_second = wtime();
+    //     react_ns_p_cell = 0.0;
+    //     vel_ns_p_cell = 0.0;
+    //     dens_ns_p_cell = 0.0;
+    //     times = 1;
+    // } else {
+    //     times++;
+    // }
 }
 
 
@@ -207,9 +212,13 @@ int main(int argc, char** argv)
         exit(1);
     }
     clear_data();
+    static double start_t = 0.0;
+    start_t = wtime();
     for (i = 0; i < 2048; i++) {
         one_step();
     }
+    printf("Total cells processed %li\n", total_cells_processed);
+    printf("%lf - ns per cell total\n",1.0e9*(wtime()-start_t)/total_cells_processed);
     free_data();
 
     exit(0);
