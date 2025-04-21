0d884c80c3f4f1e38bfe0e9a6487ef4c3d92513d
CP2025 <cp2025_003@debian-BULLSEYE-live-builder-AMD64>
2025-04-21 03:15:33 -0300

    Blocking

--- solver.c
+++ solver.c
@@ -39,8 +39,8 @@ static void set_bnd(unsigned int n, boundary b, float* x)
 static void lin_solve(unsigned int n, boundary b, float* x, const float* x0, float a, float c)
 {
     for (unsigned int k = 0; k < 20; k++) {
-        for (unsigned int i = 1; i <= n; i++) {
-            for (unsigned int j = 1; j <= n; j++) {
+        for (unsigned int j = 1; i <= n; i++) {
+            for (unsigned int i = 1; j <= n; j++) {
                 x[IX(i, j)] = (x0[IX(i, j)] + a * (x[IX(i - 1, j)] + x[IX(i + 1, j)] + x[IX(i, j - 1)] + x[IX(i, j + 1)])) / c;
             }
         }
